466c33b90cf4cd272eb1f83a21482a8e1439dbd4
dengyun-git <53471532+dengyun-git@users.noreply.github.com>
2021-05-12 14:18:02 +0100

    Update sampleTypeBreakDown.R

--- DAPtranche1/sampleTypeBreakDown.R
+++ DAPtranche1/sampleTypeBreakDown.R
@@ -2,12 +2,6 @@
 inputfile1 <- paste(myFilePath,"SS-200008.ADat",sep="")
 RawM1 <- read.adat(inputfile1)
 
-SampleID1 = grep("STEP",RawM1[,"SampleId"])
-length(SampleID1)  ###435
-SampleDat1 = RawM1[,"SampleId"][SampleID1]
-SampleDat11 = sub("-F-V[[:alnum:]]*-HT*","",SampleDat1)
-length(which(duplicated(SampleDat11)==TRUE))  ###no duplicated samples
-
 nonSampleID1 = which(!grepl("STEP",RawM1[,"SampleId"])==TRUE)
 length(nonSampleID1) ### 45
 nonSampleDat1 = RawM1[,"SampleId"][nonSampleID1]
@@ -18,6 +12,12 @@ length(which(grepl("INJ POOL-HT",RawM1[,"SampleId"])==TRUE))
 # INJ POOL-H = 7
 # UNSP POOL = 0
 
+### longitudinal sample check
+SampleID1 = grep("STEP",RawM1[,"SampleId"])
+length(SampleID1)  ###435
+SampleDat1 = RawM1[,"SampleId"][SampleID1]
+SampleDat11 = sub("-F-V[[:alnum:]]*-HT*","",SampleDat1)
+length(which(duplicated(SampleDat11)==TRUE))  ###no duplicated samples
 
 ## Tranche2 sample type breakdown
 clinic <- read_excel("/Users/ydeng/Documents/QCstepOA/clinic/STepUP_QCData_Tranche2_09MAR2021.xlsx", col_names = TRUE)
@@ -26,9 +26,21 @@ clinicSTEP = clinic$...1
 inputfile2 <- paste(myFilePath,"SS-205086.adat",sep="")
 RawM2 <- read.adat(inputfile2)
 
+nonSampleID2 = which(!grepl("STEP",RawM2[,"SampleId"])==TRUE)
+length(nonSampleID2) ### 61
+nonSampleDat2 = RawM2[,"SampleId"][nonSampleID2]
+levels(as.factor(nonSampleDat2))
+
+length(which(grepl("UNSP POOL",RawM2[,"SampleId"])==TRUE))
+# OA POOL-HT = 8
+# INJ POOL-H = 7
+# UNSP POOL = 4
+unPoolID = grep("UNSP POOL",nonSampleDat2)
+
+###longitudinal sample check
 SampleID2 = grep("STEP",RawM2[,"SampleId"])
 length(SampleID2)  ###611
-SampleDat2 = RawM2[,"SampleId"][SampleID]
+SampleDat2 = RawM2[,"SampleId"][SampleID2]
 
 ### remove spuns
 SampleDat22 =SampleDat2[!grepl("SP",SampleDat2)==TRUE]
@@ -52,10 +64,11 @@ length(which(duplicated(SampleDat222)==TRUE)) ###32
 tranche2DupSTEP = which(duplicated(SampleDat222)==TRUE)
 tranche2DupDat = SampleDat222[tranche2DupSTEP]
 
+#IDclinicT2 = vector(mode="list",length=length(tranche2DupDat))
 IDclinicT2 = vector(mode="integer",length=length(tranche2DupDat))
 for (longCounter in 1:length(tranche2DupDat)){
   IDclinicT2[longCounter] = grep(tranche2DupDat[longCounter],clinicSTEP)[1]
-}
+} ### 3 injury samples measured 3 times.
 
 longType = clinic$...3[IDclinicT2]
 ###longitudinal OA
@@ -63,19 +76,7 @@ length(grep("OA",longType))  ###18
 ###longitudinal Injury
 length(grep("Injury",longType) )###14
 ### if tranche2 only, baseline OA: 532-18 =514
-### if tranche2 onlu, baseline injury: 72-14 =58
-
-
-nonSampleID2 = which(!grepl("STEP",RawM2[,"SampleId"])==TRUE)
-length(nonSampleID2) ### 61
-nonSampleDat2 = RawM2[,"SampleId"][nonSampleID2]
-levels(as.factor(nonSampleDat2))
-
-length(which(grepl("UNSP POOL",RawM2[,"SampleId"])==TRUE))
-# OA POOL-HT = 8
-# INJ POOL-H = 7
-# UNSP POOL = 4
-unPoolID = grep("UNSP POOL",nonSampleDat2)
+### if tranche2 only, baseline injury: 72-14 -3 =55
 
 
 ###combine two tranches data
@@ -87,8 +88,9 @@ STEPdat = RawM12SampleType[STEP]
 STEPdat2 = sub("-..*","",STEPdat)
 length(STEPdat2) ##1046
 length(unique(STEPdat2))
+
 ### duplicated STEPIDs 
-longSamp = STEPdat2[duplicated(STEPdat2)] 
+longSamp = unique(STEPdat2[duplicated(STEPdat2)]) 
 
 duplicatedTimes=vector()
 for (ii in 1:length(longSamp)){
@@ -100,7 +102,7 @@ duplicated3STEP = longSamp[duplicated3ID] ### sample name measured for 3 times
 
 IDclinic = vector(mode="integer",length=length(longSamp))
 for (longCounter in 1:length(longSamp)){
-  IDclinic[longCounter] = grep(longSamp[longCounter],clinicSTEP)
+  IDclinic[longCounter] = grep(longSamp[longCounter],clinicSTEP)[1]
 }
 
 
@@ -108,7 +110,7 @@ longType = clinic$...3[IDclinic]
 ###longitudinal OA
 length(grep("OA",longType))  ###36
 ###longitudinal Injury
-length(grep("Injury",longType) )###28
+length(grep("Injury",longType) )###25
 
 ### extract sample type for duplicated sample of 3 times
 for (longCounter in 1:length(duplicated3STEP)){
@@ -117,8 +119,8 @@ for (longCounter in 1:length(duplicated3STEP)){
 longType = clinic$...3[IDclinic]
 length(grep("OA",longType))  ###0
 ###longitudinal Injury
-length(grep("Injury",longType) )###6
+length(grep("Injury",longType) )###3
 
-### if tranche1 and 2 together, baseline OA 258 + 532 -36 = 764
-### if tranche1 and 2 together, baseline Injury 175 + 72 -28 -6 = 213
+### if tranche1 and 2 together, baseline OA 258 + 532 -36 = 754
+### if tranche1 and 2 together, baseline Injury 175 + 72 -28 -3  = 216
 
