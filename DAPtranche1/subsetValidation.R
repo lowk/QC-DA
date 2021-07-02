dc588201ff0c856c2850e0ba552394fafee3bc0c
dengyun-git <53471532+dengyun-git@users.noreply.github.com>
2021-07-02 13:39:15 +0100

    Update subsetValidation.R

--- DAPtranche1/subsetValidation.R
+++ DAPtranche1/subsetValidation.R
@@ -2,6 +2,7 @@ library(MASS)
 library(NbClust)
 library(mclust)
 
+### calculate fks corresponding to cluster number 1:10
 fks = vector(mode="numeric",length=10)
 for (myK in 1:10)
 {fks[myK] <-FkStatistic(pcDat,myK)[[1]]}
@@ -16,25 +17,27 @@ if(!any(fks<0.85)){print("only 1 cluster detected")
   
   ClustType <- kmeanStr$cluster
   
-  ###10-cross validation, using subsetting
+  ###10-cross validation
   sampID <- 1:nrow(pcDat)
   similairyVK = vector("integer",length=10)
   
   for (validK in 1:10){
     testID <- seq(10*(validK-1)+1,10*validK)
     trainID <- which(!(sampID%in%testID))
-    TrKCenter <- kmeans(pcDat[trainID,],BestCN,iter.max = 100, nstart = 1)$centers
+    TrKCenter <- kmeans(pcDat[trainID,],BestCN, iter.max = 100, nstart = 1)$centers
     
     testClusterType= vector("integer",length=length(testID))
     for (testCounter in 1:length(testID)){
       
       distanceK = vector("integer",length=nrow(TrKCenter))
+###assign cluster membership by finding the nearest center
       for(centerCounter in 1:nrow(TrKCenter)){
         distanceK[centerCounter] = dist(rbind(pcDat[testID[testCounter],],TrKCenter[centerCounter,]))
       }
-      
+### testClusetType: assigned by the nearest center from the test data
       testClusterType[testCounter] <- which(distanceK==min(distanceK))
     }
+### similarity between "test data" and "orignial clustering based on the whole data".
     similairyVK[validK] =  mclust::adjustedRandIndex(ClustType[testID],testClusterType)
   }
   
