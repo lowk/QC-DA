2904a82d6b32318ca667b3d105148f4a278ff32a
dengyun-git <53471532+dengyun-git@users.noreply.github.com>
2021-10-04 13:16:51 +0100

    Add files via upload

--- subanalysis1.1.R
+++ subanalysis1.1.R
@@ -7,51 +7,14 @@ library(fpc)
 library(WGCNA)
 library(sparcl)
 library(diceR)
+source("callMe.R")
 
 # library(pvclust) ### consider which package best display dendrogram
 
 ### Perform all the analysis in OA injury groups separately, unless otherwise specified.
-
-filterHM <- function(MySoma,BioMeta){
-  HMpro <- which(!grepl("HybControlElution|Non",colnames(MySoma)))
-  HMsam <- which(grepl("Sample",MySoma[,"SampleType"]))
-  MySomaDone <- MySoma[HMsam,HMpro]
-  BioMetaDone <- BioMeta[HMsam,]
-  return(list(MySomaDone,BioMetaDone))
-}
-
-### get plate batch
-GetPlateBatch <- function(MySomaPlate){
-  plates <- levels(as.factor(MySomaPlate))
-  
-  PlateBatch = matrix(0,ncol=1,nrow=length(MySomaPlate))
-  for(palteCounter in 1:length(MySomaPlate)){
-    PlateBatch[palteCounter] = which(plates == MySomaPlate[palteCounter])
-  }
-  return(PlateBatch)
-}
-
-#f(K) statistics
-getALFA <- function(thisk,Nd){
-  if (thisk == 2) {temp = 1 - 3/(4*Nd)}
-  else{temp <- getALFA(thisk-1,Nd) + (1-getALFA(thisk-1, Nd))/6}
-  return(temp)
-}
-
-FkStatistic <- function(pcDat,thisk){
-  Nd = ncol(pcDat)
-  kmeanStr <- kmeans(pcDat, thisk, iter.max = 50, nstart = 1) ###?iter.max should be???
-  Sk = kmeanStr$tot.withinss
-  if(thisk==1){fs=1}
-  else if(FkStatistic(pcDat,thisk-1)[[2]]==0){fs=1}
-  else{fs = Sk/(getALFA(thisk,Nd)*FkStatistic(pcDat,thisk-1)[[2]])}
-  return(list(fs,Sk))
-}
-
-
-# Calculate PCA
 load("sourceData.RData")
 
+# Calculate PCA
 ### exclude none human samples and none human proteins
 MySoma1Done <- filterHM(MySoma1,BioMeta1)[[1]]
 MySoma2Done <- filterHM(MySoma2,BioMeta2)[[1]]
