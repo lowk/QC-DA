b455e6d4c149bcaa6e8c2d04321c6af3a5246794
dengyun-git <53471532+dengyun-git@users.noreply.github.com>
2021-10-07 17:26:50 +0100

    Add files via upload

--- DAPtranche1/QCnormTranche2.R
+++ DAPtranche1/QCnormTranche2.R
@@ -1,5 +1,36 @@
-### RFUs Normalisation, across all the plates
+### filter to exclude buffer|calibrator, select only clinical samples and human proteins
+### input RFU matrix after normalisation steps, output filtered RFU matrix only with human data 
+filterHM <- function(MySoma,BioMeta){
+  HMpro <- which(!grepl("HybControlElution|Non",colnames(MySoma)))
+  HMsam <- which(grepl("Sample",MySoma[,"SampleType"]))
+  MySomaDone <- MySoma[HMsam,HMpro]
+  BioMetaDone <- BioMeta[HMsam,]
+  return(list(MySomaDone,BioMetaDone))
+}
 
+### get plate batch
+GetPlateBatch <- function(MySomaPlate){
+  plates <- levels(as.factor(MySomaPlate))
+  
+  PlateBatch = matrix(0,ncol=1,nrow=length(MySomaPlate))
+  for(palteCounter in 1:length(MySomaPlate)){
+    PlateBatch[palteCounter] = which(plates == MySomaPlate[palteCounter])
+  }
+  return(PlateBatch)
+}
+
+### check my code accuracy by comparing our normalisation and somalogic provided normalised data
+checkMyCode <- function(RawM,SomaFile){
+  SomaM <- read.adat(SomaFile)
+  FunList = list(HYBNORM,MIDNORMcali,PLATESCALE,MIDNORMsamp)
+  MySoma = UserNorm(FunList,RawM)
+  corShip = CompTWO(SomaM,MySoma) 
+  # DatStartId <- which(colnames(MySoma)=="CRYBB2.10000.28")
+  # colnames(MySoma[DatStartId:ncol(MySoma)])[which(corShip<0.7)] ### different analyses
+}
+
+
+### RFUs Normalisation, across all the plates
 getMySoma <- function(Platelist){
   MySoma = data.frame(Platelist[[1]]) ###MySoma: the whole dataframe for all the plates
   for (plateC in 2:length(Platelist)){
@@ -129,90 +160,6 @@ CALIBRATION <- function(RawM){
   return(MySoma)
 }
 
-### 4. Median Normalization. Input: RFU whole data frame, user defined sample type;
-###Output: median normalised (row-wise and column-wise) RFU whole data frame
-# 
-# MIDNORM = function(RawM,caliCase){ ###caliCase control which sample type to be applied MidNorm
-#   
-#   PlateIdUni = levels(factor(RawM$PlateId))
-#   
-#   Platelist = list()
-#   
-#   for (plateCounter in 1:length(PlateIdUni)){
-#     
-#     PlateIdSg = which(RawM$PlateId == PlateIdUni[plateCounter])
-#     
-#     RawMS = RawM[PlateIdSg,] ### single plate
-#     
-#     idHyb = which(grepl("HybControlElution",colnames(RawMS))==TRUE)
-#     idNonHyb = which(!grepl("HybControlElution",colnames(RawMS))==TRUE)
-#     RawMS1 = RawMS[,idNonHyb] ###RawM1 to track "Ratio of Normalization Median to Sample Value"
-#     DatStartId = which(colnames(RawMS1) == "RMA") +1
-#     DatStartIdP = which(colnames(RawMS) == "RMA") +1
-#     
-#     sampTypePre = levels(factor(RawMS$SampleType))
-#     if (caliCase == 1){sampType = "Calibrator"}
-#     else{sampType = sampTypePre[which(sampTypePre!="Calibrator")]}    
-#     
-#     for (sampTypeCounter in 1:length(sampType)){
-#       
-#       idSamp = which(RawMS1$SampleType == sampType[[sampTypeCounter]])
-#       
-#       datZoneP = RawMS1[,DatStartId:ncol(RawMS1)] ### datazone excludes "HybControlElution"
-#       
-#       datZone = RawMS1[idSamp,DatStartId:ncol(RawMS1)]  ###dataZone only include interested sample type
-#       
-#       if(length(idSamp)==1) {SampTypeRFU = matrix(datZone,nrow=1)
-#       SampTypeMedian = SampTypeRFU
-#       SampMedianNorm = as.matrix(SampTypeMedian/SampTypeRFU)}
-#       
-#       else {SampTypeRFU = datZone
-#       SampTypeMedian = apply(SampTypeRFU,2,median)
-#       SampMedianNorm = t(apply(SampTypeRFU,1,function(x){SampTypeMedian/x}))}
-#       
-#       Dilute = Dilution[which(Dilution!="0")] ###SampMedianNorm & Dilute: ncol(SampMedianNorm)=length(Dilute)
-#       uniqDilute = levels(factor(Dilute))
-#       
-#       for (idDilute in (1:length(uniqDilute))){
-#         
-#         DataDiluteID = which(Dilute==uniqDilute[idDilute])
-#         
-#         MedianDiluteSingle = matrix(apply(SampMedianNorm[,DataDiluteID],1,median),ncol=1)
-#         
-#         DataDiluteNormT = apply(datZone[,DataDiluteID],2,function(x){MedianDiluteSingle*x})
-#         
-#         if (idDilute==1){DataDiluteNorm = DataDiluteNormT}
-#         else{DataDiluteNorm = cbind(DataDiluteNorm,DataDiluteNormT)}
-#       }
-#       
-#       if (sampTypeCounter==1) {RawMStemp = cbind(RawMS[idSamp,1:(DatStartIdP-1)],RawMS[idSamp,idHyb],DataDiluteNorm)}
-#       else{
-#         RawMStempT = cbind(RawMS[idSamp,1:(DatStartIdP-1)],RawMS[idSamp,idHyb],DataDiluteNorm)
-#         RawMStemp = rbind(RawMStemp,RawMStempT)
-#       }
-#     }
-#     
-#     Platelist[[plateCounter]] = RawMStemp  
-#   }
-#   
-#   MySomaTemp = getMySoma(Platelist)  
-#   
-#   ###up to date MySomaTemp has different indexing from Raw. We make them consistent
-#   rowOrderName = rownames(RawM)
-#   rowOrder = vector(mode="numeric",length=nrow(MySomaTemp))
-#   for (j in 1:nrow(MySomaTemp)){
-#     rowOrder[j] = which(rownames(MySomaTemp) %in% rowOrderName[j])
-#   }
-#   
-#   colOrderName = colnames(RawM)
-#   colOrder = vector(mode="numeric",length=ncol(MySomaTemp))
-#   for (k in 1:ncol(MySomaTemp)){
-#     colOrder[k] = which(colnames(MySomaTemp) %in% colOrderName[k])
-#   }
-#   
-#   MySoma = MySomaTemp[rowOrder,colOrder]
-#   return(MySoma)
-# }
 
 MIDNORM = function(RawM){ ###caliCase control which sample type to be applied MidNorm
   
@@ -462,7 +409,6 @@ initQCnorm <- function(inputfile1,inputfile2){
   
   RawM <- read.adat(inputfile1)
   ### read in Col^MetaTable
-  ###use inputfile1, there is no PlateScale_Reference information, so we use inputfile2 considering all the row and column indices are consistent
   
   con1 = file(inputfile2, "r")
   
@@ -486,17 +432,16 @@ initQCnorm <- function(inputfile1,inputfile2){
       if(slineL[getStartTerm]=="EntrezGeneSymbol"){ EntrezGeneSymbol <<- slineL[getStartId:length(slineL)]}
       if(slineL[getStartTerm]=="Type"){Type <<- slineL[getStartId:length(slineL)]}
       if(slineL[getStartTerm]=="Dilution"){Dilution <<- slineL[getStartId:length(slineL)]}
-      if(slineL[getStartTerm]=="medNormRefSMP_ReferenceRFU"){
-        PlateScale_Reference <<- as.numeric(slineL[getStartId:length(slineL)])  
-        # load("PlateScale_Reference.Rdata")
-        break} ### tranche2 data, Platesclae_Reference is labelled "medNormRefSMP_ReferenceRFU"
-      
-      ### 30 is the beginning field of the ^COL_DATA;column names of meta data = SeqId +Target
+      # if(slineL[getStartTerm]=="medNormRefSMP_ReferenceRFU"){
+      #   medNormRefSMP <<- as.numeric(slineL[getStartId:length(slineL)])
+      if(slineL[getStartTerm]=="PlateScale_Reference"){
+        PlateScale_Reference <<- as.numeric(slineL[getStartId:length(slineL)])
+        
+        break} 
     }
   }
   close(con1)
   
-  ### pay attention, tranche2 RawM protein data begin from the 26th col
   DatStartId = which(colnames(RawM) == "CRYBB2.10000.28")
   ColTable <- cbind(as.matrix(colnames(RawM)[DatStartId:ncol(RawM)]),as.matrix(UniProt),as.matrix(EntrezGeneID),as.matrix(EntrezGeneSymbol),as.matrix(TargetFullName),as.matrix(Target))
   colnames(ColTable) <- list("Protein Name", "UniPro ID", 'EntrezGeneID',"EntrezGeneSymbol","TargetFullName","Target")
@@ -568,7 +513,7 @@ ExtractClinicG = function(RawM,inputfile,trancheT){
   bloodStainInfor[bloodStainInfor=="-"]<-NA
   
   ###sampleAge of tranche2 excel, we need to format: Data->text to column + general   
-  ###01 JAN 2021 = 44197 as our uptodate
+  ###01 JAN 2021 = 44197 as our up to date
   sampleAgeInfor <- 44197 - metadata$`Date of biological sampling`
   
   patientGenderInfor = metadata$`Patient gender`
@@ -619,14 +564,8 @@ ExtractClinicG = function(RawM,inputfile,trancheT){
     stepupIDTRaw[selPatient] = STEPupName[spCounter]
   } ###RawM row order didn't change, just added more information from STEpUP_QCData_Tranche1.xlsx
   
-  # DatStartId <- which(colnames(RawM)=="CRYBB2.10000.28")
-  # DiseaseType <- levels(as.factor(GroupTRawM))
-  # for (Dcounter in 1:length(DiseaseType)){
-  #   RawM$SampleType[which(GroupTRawM==DiseaseType[Dcounter])]=paste(DiseaseType[Dcounter],"Sample",sep="")
-  #   
-  # }
   
-  ### up to now integrate disease group type into RawM, RawM$SampleType including Pool type, disease sample type
+  ### up to here integrate disease group type into RawM, RawM$SampleType including Pool type, disease sample type
   stepupIDTRaw[stepupIDTRaw == "STEP1409F-V1-HT1"] <- "STEP1409-F-V1-HT1"
   
   BioMeta = cbind(RawM$PlateId,stepupIDTRaw,GroupTRawM,CohortTRawM,bloodStainTRawM,sampleAgeTRaw,patientGenderTRaw,patientAgeTRaw)
@@ -634,4 +573,101 @@ ExtractClinicG = function(RawM,inputfile,trancheT){
   rownames(BioMeta) = rownames(RawM)
   
   return(BioMeta) 
+}
+
+### k batch for KNN testing: input expression data and batch matrix, return rejection rate matrix
+KNNtest <- function(exprDat_norm,BioMeta,myRound,kBatch){
+  
+  ### k nearest neighborhood batch effect test, average among myRound, for kBatch. Per round includes different sampSizeS, and kNearS
+  BatchEffectM = vector(mode="list",length=myRound)
+  
+  pc_norm <- prcomp(exprDat_norm,scale = TRUE)
+  topPCn <- which(get_eigenvalue(pc_norm)$cumulative.variance.percent>80)[1]
+  distPairs = pc_norm$x[,1:topPCn]
+  DisM = as.matrix(dist(distPairs, method = "euclidean", diag = TRUE, upper = TRUE))
+  
+  sampSizeS = seq(10,500,by=100)  ### different percentage of 1%~25% samples for batch effect test (based on the paper)
+  kNearS = seq(10,500,by=100)          ### different neiboughood sizes, based on testing, >250 no rejection at all
+  
+  for (roundCount in 1:myRound){
+    positiveRate = matrix(NA,nrow=length(kNearS),ncol=length(sampSizeS))
+    batchTest = vector(mode="list",length=kBatch)
+    
+    for (batchCf in 1:kBatch){ 
+      tblWh = table(BioMeta[,batchCf])
+      EpectedRio = tblWh/sum(tblWh)
+      
+      for (sampSct in 1:length(sampSizeS)){
+        sampSize = sampSizeS[sampSct]
+        randSampId = sample(1:nrow(exprDat_norm),sampSize) ### random select which samples to be tested
+        
+        for (neiSize in 1:length(kNearS)){
+          
+          kNear = kNearS[neiSize]   ###set neighborhood 
+          
+          idInMetaR = matrix(NA,nrow=kNear-1,ncol=1)
+          testSampYN = matrix(NA,nrow=sampSize,ncol=1)
+          
+          
+          for (testP in  1:sampSize){
+            
+            testSamp = DisM[randSampId[testP],]  ###define test sample
+            neighborD = sort(testSamp)[2:kNear]  ### find its nearest kNear neighbors
+            neiborOriID = names(neighborD)       ### find its neighbor name, for batch label matching
+            
+            neiNameList = rownames(BioMeta)
+            
+            for (neiCounter in 1:(kNear-1)){ 
+              idInMetaR[neiCounter] = which(neiNameList %in% neiborOriID[neiCounter]) ### meta for tested sample
+            }
+            
+            neiDat = BioMeta[idInMetaR,batchCf]
+            tblPart = table(neiDat)
+            
+            missingCtg = apply(as.matrix(rownames(as.matrix(tblPart))),2,function(x){which(!(rownames(as.matrix(tblWh)) %in% x))})
+            if (length(missingCtg) != 0) {### for categories with 0 count
+              missingVl = matrix(0,ncol=length(missingCtg))
+              colnames(missingVl) <- rownames(as.matrix(tblWh))[missingCtg]
+              tblPart = cbind(t(as.matrix(tblPart)),missingVl)
+              tblTest = rbind(tblPart[,rownames(as.matrix(tblWh))],tblWh) ### make col orders consistent
+            }
+            
+            else{tblTest = rbind(tblPart,tblWh)}
+            
+            ### chi square test or exact test, check expected value for each element >5?
+            if(min(rowSums(tblTest) %*% t(colSums(tblTest)) / sum(tblTest)) >5){
+              reportYN = chisq.test(tblTest)$p.value}
+            else{reportYN = fisher.test(tblTest,simulate.p.value = TRUE)$p.value}
+            
+            ### chi square based multinomial test, power is less than above one
+            # chi.sq.value <- sum((tblTest[1,]/sampSize - EpectedRio)^2/EpectedRio)
+            #  
+            # reportYN <- 1 - pchisq(chi.sq.value, df=length(unique(MetaRaw[,batchCf]))-1) #p-value for the result
+            
+            ### hypothesis test, local batch label distribution against the global label distribution
+            if(reportYN < 0.05/sampSize) {testSampYN [testP] = 1} ###using Bonferroni adjusted p values
+            else {testSampYN[testP] = 0}
+            
+          } 
+          
+          positiveRate[neiSize,sampSct] = length(which(testSampYN == 1)) / length(testSampYN)
+          colnames(positiveRate) = sampSizeS
+          rownames(positiveRate) = kNearS
+          batchTest[[batchCf]]=positiveRate ### columns: samples for test, rows: neighborhood
+          
+        }
+        
+      }
+      
+    }
+    BatchEffectM[[roundCount]] = sapply(batchTest,mean)
+  }
+  
+  BatchEffectMM = matrix(unlist(BatchEffectM),nrow=2,byrow=TRUE)
+  meanM = apply(BatchEffectMM,2,mean)
+  sdM = apply(BatchEffectMM,2,sd)
+  BatchEffect = rbind(meanM,sdM)
+  colnames(BatchEffect) = colnames(BioMeta)
+  
+  return(BatchEffect)
 }
